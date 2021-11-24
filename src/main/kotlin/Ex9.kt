a8ef5728a7bdb5bc36eb697d3e8f25ffae54d5ff
HousniAbdellatife <abdellatife.housni@gmail.com>
2021-11-24 20:00:11 +0100

    Data classes

--- src/main/kotlin/Ex9.kt
+++ src/main/kotlin/Ex9.kt
@@ -4,13 +4,14 @@ package example1
  * Data classes
  */
 
-fun eval(expr: Expr): Int =
-    when (expr) {
-        is Num -> expr.value
-        is Sum -> eval(expr.left) + eval(expr.right)
-        else -> throw IllegalArgumentException("Unknown expression")
-    }
+data class Person(val name: String, val age: Int)
 
-interface Expr
-class Num(val value: Int) : Expr
-class Sum(val left: Expr, val right: Expr) : Expr
+fun getPeople(): List<Person> {
+    return listOf(Person("Alice", 29), Person("Bob", 31))
+}
+
+fun comparePeople(): Boolean {
+    val p1 = Person("Alice", 29)
+    val p2 = Person("Alice", 29)
+    return p1 == p2  // should be true
+}
