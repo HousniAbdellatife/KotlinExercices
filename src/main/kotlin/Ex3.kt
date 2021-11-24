0c9412dd8757f6ab224cbcb462fc0742c7a0e399
HousniAbdellatife <abdellatife.housni@gmail.com>
2021-11-24 19:02:09 +0100

    Default Arguments

---
+++ src/main/kotlin/Ex3.kt
@@ -0,0 +1,18 @@
+package example1
+
+/**
+ * Default Arguments
+ */
+fun main() {
+    println(useFoo())
+}
+
+fun foo(name: String, number: Int = 42, toUpperCase: Boolean = false) =
+    (if (toUpperCase) name.uppercase() else name) + number
+
+fun useFoo() = listOf(
+    foo(name = "a"),
+    foo(name = "b", number = 1),
+    foo(name = "c", toUpperCase = true),
+    foo(name = "d", number = 2, toUpperCase = true)
+)
